037eeb1dfcbdd37a158a7f49e17f543afeeeed39
BenoitAudigier <32508114+BenoitAudigier@users.noreply.github.com>
2018-10-19 23:55:11 +0200

    Update detection_functions_using_own_model.py

--- detection_functions_using_own_model.py
+++ detection_functions_using_own_model.py
@@ -19,6 +19,8 @@ def videoReader(vidCap):
   pathToCheckpoint = modelName + '/frozen_inference_graph.pb'
   pathToModelLabels = os.path.join('trainSysnav', 'object-detection.pbtxt')
   maxNumberOfClass = 1
+  #opener = urllib.request.URLopener()
+  #opener.retrieve(url_base + modelFile, modelFile) # url_base not defined
 
   print("Load trained model")
   detection_graph = tf.Graph()
@@ -37,8 +39,15 @@ def videoReader(vidCap):
   print("Begining treatment of the video")
   with detection_graph.as_default():
     with tf.Session(graph=detection_graph) as sess:
-      while True:
-        ret, image_np = vidCap.read()
+        
+        
+      for image_name in os.listdir('sysnav/imagesTest/'):
+#      while True:
+#        ret, image_np = vidCap.read()
+        try:
+            image_np = cv2.imread('sysnav/imagesTest/' + image_name)
+        except:
+            continue
         # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
         image_np_expanded = np.expand_dims(image_np, axis=0)
         image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
@@ -48,29 +57,61 @@ def videoReader(vidCap):
         # Score is shown on the result image, together with the class label.
         scores = detection_graph.get_tensor_by_name('detection_scores:0')
         classes = detection_graph.get_tensor_by_name('detection_classes:0')
-        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
         # Actual detection.
-        (boxes, scores, classes, num_detections) = sess.run(
-            [boxes, scores, classes, num_detections],
+        (boxes, scores, classes) = sess.run(
+            [boxes, scores, classes],
             feed_dict={image_tensor: image_np_expanded})
+        # Applying threshold
+        boxes = keepRelevantBoxes(boxes, scores)
+        boxes = np.array([convertToCoordinates(box, image_np.shape[0:2]) for box in boxes])
         # Visualization of the results of a detection.
-        print(boxes)
-        print(scores)
-        print(num_detections)
         vis_util.visualize_boxes_and_labels_on_image_array(
             image_np,
-            np.squeeze(boxes),
+            boxes,
             np.squeeze(classes).astype(np.int32),
             np.squeeze(scores),
             category_index,
-            use_normalized_coordinates=True,
+#            use_normalized_coordinates=True,
+            use_normalized_coordinates=False,
             line_thickness=4)
 
         cv2.imshow('object detection', cv2.resize(image_np, (1100,600)))
-        time.sleep(.3)
+        time.sleep(1)
+
         if cv2.waitKey(25) & 0xFF == ord('q'):
           cv2.destroyAllWindows()
           break
+      
+        # to remove later
+#        cv2.destroyAllWindows()
+        
+def keepRelevantBoxes(boxes, scores, threshold = .5):
+    scores = scores[scores>threshold] # the list is already sorted
+    num_predictions = len(scores)
+    return boxes[0][0:num_predictions]
+
+
+def convertToCoordinates(box, im_dim):
+    im_height, im_width = im_dim
+    xmin = int(box[0] * im_height)
+    xmax = int(box[1] * im_width)
+    ymin = int(box[2] * im_height)
+    ymax = int(box[3] * im_width)
+    return(np.array([xmin, xmax, ymin, ymax]))
+
+
+def convertToCoordinatesTmp(box, im_dim):
+    im_height, im_width = im_dim
+    xmax = int(box[0] * im_width)
+    xmin = int(box[1] * im_width)
+    ymax = int(box[2] * im_height)
+    ymin = int(box[3] * im_height)
+    return(np.array([[xmin, ymin], [xmax, ymax], [xmax, ymin], [xmin, ymax]]))
+
+
+
+
 
 v = cv2.VideoCapture('sysnav/positive.avi')
 videoReader(v)
+
